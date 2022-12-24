213c75502d12b7d0b53267ca0a86dc832835facb
Matthew <mrsenormatty@live.com>
2022-12-23 16:07:30 -0800

    code refactoring

--- index.js
+++ index.js
@@ -4,25 +4,33 @@
 1 kilogram = 2.204 pound
 */
 
-const inputEl = document.getElementById('unit-value')
-const btn = document.getElementById('convert-btn')
-const volumeEl = document.getElementById('volume-el')
-const lengthEl = document.getElementById('length-el')
-const massEl = document.getElementById('mass-el')
+const inputEl = document.getElementById('unit-value');
+const btn = document.getElementById('convert-btn');
+const volumeEl = document.getElementById('volume-el');
+const lengthEl = document.getElementById('length-el');
+const massEl = document.getElementById('mass-el');
 
 function displayCalc() {
-    let value = inputEl.value;
-    let metLength = value * 3.281
-    let impLength = value / 3.281
-    let metVolume = value * .264
-    let impVolume = value / .264
-    let metMass = value * 2.204
-    let impMass = value / 2.204  
-    lengthEl.textContent = `${value} meters = ${metLength.toFixed(3)} feet | ${value} feet = ${impLength.toFixed(3)} meters`
-    volumeEl.textContent = `${value} liters = ${metVolume.toFixed(3)} gallons | ${value} gallons = ${impVolume.toFixed(3)} liters`
-    massEl.textContent = `${value} kilograms = ${metMass.toFixed(3)} pounds | ${value} pounds = ${impMass.toFixed(3)} kilograms`
+	let value = inputEl.value;
+	let metLength = value * 3.281;
+	let impLength = value / 3.281;
+	let metVolume = value * 0.264;
+	let impVolume = value / 0.264;
+	let metMass = value * 2.204;
+	let impMass = value / 2.204;
+	lengthEl.textContent = `${value} meters = ${metLength.toFixed(
+		3,
+	)} feet | ${value} feet = ${impLength.toFixed(3)} meters`;
+	volumeEl.textContent = `${value} liters = ${metVolume.toFixed(
+		3,
+	)} gallons | ${value} gallons = ${impVolume.toFixed(3)} liters`;
+	massEl.textContent = `${value} kilograms = ${metMass.toFixed(
+		3,
+	)} pounds | ${value} pounds = ${impMass.toFixed(3)} kilograms`;
 }
-// When user click convert, generate: 
-btn.addEventListener ('click', () => {
-    displayCalc()
-})
+
+// When user clicks convert, generate:
+btn.addEventListener('click', () => {
+	displayCalc();
+	inputEl.value = '';
+});
